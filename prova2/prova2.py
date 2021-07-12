2d352b3e3d3e431ab4a336911d7b2d86eeb625fa
Richard Gonzalez <richardmatteus96@gmail.com>
2021-07-12 19:37:26 -0300

    commit da segunda prova

--- prova2/prova2.py
+++ prova2/prova2.py
@@ -1,48 +1,65 @@
+class Livro:
+    def __init__(self, id, titulo, autor):
+        self.id = id
+        self.titulo = titulo
+        self.autor = autor
+
 class Autor:
     def __init__(self, id, nome):
         self.__id = id
         self.nome = nome
 
-class Laco:
-    def __init__(self, dado):
-        self.dado = dado
-        self.next = None
-        
-class Pilha:
-    def _init_(self):
-        self.topo = None
-        self.range = 0
+    def imprimir(self):
+        print("Id do autor:", self.__id)
+        print("Nome do autor:", self.nome)
 
-   
-    def empilhar(self, elemento):
-        laco = Laco(elemento)
-        laco.next = self.topo
-        self.topo = laco
-        self.range += 1
+class Pilha(object):
+    def __init__(self):
+        self.dados = []
+ 
+    def empilha(self, elemento):
+        self.dados.append(elemento)
+ 
+    def desempilha(self):
+        if not self.vazia():
+            return self.dados.pop(-1)
 
-    def remover(self):
-        if self.range > 0:
-            laco = self.topo
-            self.topo = self.topo.next
-            self.range -= 1
-            return laco.dado
-        print("A pilha está vazia")    
+    def vazia(self):
+        return len(self.dados) == 0
+ 
+    def imprimir(self):
+        if self.dados == []:
+            print('Pilha vazia.')
+        print(self.dados)
+    
+############################
+    
+a1 = Autor('1','Richard')
+a2 = Autor('2','Ana')
+a3 = Autor('3','Mauricio') 
 
-    def _tamanho_(self):
-        return self.range
+a1.imprimir()
+a2.imprimir()
+a3.imprimir()
 
-    def olhar(self):
-        if self.range > 0:
-            return self.topo.dado
-        print("A pilha está vazia")
 
-    def _reproduzir_(self):
-        r = ""
-        cursor = self.topo
-        while cursor:
-            r = r + str(cursor.dado) + "\n"
-            cursor = cursor.proximo
-        return r
 
-    def _string_(self):
-        return self._reproduzir_()
+l1 = Livro('10','Sonho de Verão',a1.nome)
+l2 = Livro('11','Sonho de Primavera',a2.nome)
+l3 = Livro('12','Sonho de Outono',a3.nome)
+
+pilha = Pilha()
+pilha.empilha(l1.titulo)
+pilha.imprimir()
+input()
+pilha.empilha(l2.titulo)
+pilha.imprimir()
+input()
+pilha.empilha(l3.titulo)
+pilha.imprimir()
+input()
+pilha.desempilha()
+pilha.imprimir()
+input()
+pilha.desempilha()
+pilha.imprimir()
